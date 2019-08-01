1c901079e1c815144d6f11382e4235c91dca7c46
m@pinehq.com <m@pinehq.com>
2019-08-01 19:12:25 +0300

    Simplify make-invoice

--- make-invoice.py
+++ make-invoice.py
@@ -8,14 +8,18 @@ import requests
 def generate_invoice(total_hours, invoice_number):
     with open('invoice.json', 'r') as f:
         invoice_data = json.load(f)
-        invoice_data['items'][0]['quantity'] = total_hours
-        invoice_data['number'] = invoice_number
-        r = requests.post('https://invoice-generator.com', json=invoice_data, stream=True)
-        if r.status_code == 200:
-            filename = f'invoice-{invoice_number}.pdf'
-            with open(filename, 'wb') as f:
-                shutil.copyfileobj(r.raw, f)
-        print(f'Invoice {invoice_number} has been successfully generated!')
+
+    invoice_data['items'][0]['quantity'] = total_hours
+    invoice_data['number'] = invoice_number
+    r = requests.post(
+        'https://invoice-generator.com', json=invoice_data, stream=True)
+    from_name = invoice_data['from'].replace(' ', '_')
+
+    if r.status_code == 200:
+        filename = f'invoice-{from_name}-{invoice_number}.pdf'
+        with open(filename, 'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+    print(f'Invoice {invoice_number} has been successfully generated!')
 
 
 def main():
@@ -31,7 +35,7 @@ def main():
         generate_invoice(total_hours, invoice_number)
     except ValueError:
         print('ERROR: Make sure that first argument you pass is a number.\n'
-              'Example: ./make-invoice.py 30.50 INV-001')
+              'Example: ./make-invoice.py 30.50 INV-2019-03')
 
 
 if __name__ == '__main__':
