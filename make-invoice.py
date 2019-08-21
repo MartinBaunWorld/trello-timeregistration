84108ba4443b759b97bfa15bae3fbb1cdeeeae89
user <m@pinehq.com>
2019-08-21 16:06:17 +0300

    Make invoicer even nicer

--- make-invoice.py
+++ make-invoice.py
@@ -5,37 +5,35 @@ import shutil
 import requests
 
 
-def generate_invoice(total_hours, invoice_number):
-    with open('invoice.json', 'r') as f:
+def generate_invoice(filename):
+    with open(filename, 'r') as f:
         invoice_data = json.load(f)
 
-    invoice_data['items'][0]['quantity'] = total_hours
-    invoice_data['number'] = invoice_number
     r = requests.post(
         'https://invoice-generator.com', json=invoice_data, stream=True)
-    from_name = invoice_data['from'].replace(' ', '_')
+    invoice_number = invoice_data['number']
 
     if r.status_code == 200:
-        filename = f'invoice-{from_name}-{invoice_number}.pdf'
-        with open(filename, 'wb') as f:
+        with open(filename.split('.json')[0] + '.pdf', 'wb') as f:
             shutil.copyfileobj(r.raw, f)
     print(f'Invoice {invoice_number} has been successfully generated!')
 
 
 def main():
     args = sys.argv[1:]
-    if len(args) < 2:
-        print('ERROR: You need to pass two arguments to generate an invoice '
-              ' - number of hours and invoice number')
+    if len(args) != 1:
+        print('ERROR: You need to pass one argument to generate an invoice '
+              ' - invoice json')
         return False
 
     try:
-        total_hours = round(float(args[0]), 2)
-        invoice_number = args[1]
-        generate_invoice(total_hours, invoice_number)
+        filename = args[0]
+        generate_invoice(filename)
     except ValueError:
-        print('ERROR: Make sure that first argument you pass is a number.\n'
-              'Example: ./make-invoice.py 30.50 INV-2019-03')
+        from traceback import format_exc
+        print(format_exc())
+        print('ERROR: Make sure that first argument you pass is the invoice data.\n'
+              'Example: ./make-invoice.py invoice-1904.json')
 
 
 if __name__ == '__main__':
